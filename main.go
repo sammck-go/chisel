1e7f0a7d049d63db7e68e5c3237b65fa191d5d22
Sam McKelvie <smckelvie@xevo.com>
2019-12-11 13:32:32 -0800

    move package to github.com/XevoInc/chisel

--- main.go
+++ main.go
@@ -8,9 +8,9 @@ import (
 	"os"
 	"strconv"
 
-	"github.com/jpillora/chisel/client"
-	"github.com/jpillora/chisel/server"
-	chshare "github.com/jpillora/chisel/share"
+	"github.com/XevoInc/chisel/client"
+	"github.com/XevoInc/chisel/server"
+	chshare "github.com/XevoInc/chisel/share"
 )
 
 var help = `
@@ -23,7 +23,7 @@ var help = `
     client - runs chisel in client mode
 
   Read more:
-    https://github.com/jpillora/chisel
+    https://github.com/XevoInc/chisel
 
 `
 
@@ -76,7 +76,7 @@ var commonHelp = `
     ` + chshare.BuildVersion + `
 
   Read more:
-    https://github.com/jpillora/chisel
+    https://github.com/XevoInc/chisel
 
 `
 
